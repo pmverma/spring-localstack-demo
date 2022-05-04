3a324b25852e39a42f647f479c59fc38306b20d2
P. Mohan <p.mohan@kbzbank.com>
2022-05-04 15:07:41 +0630

    Initial commit

---
+++ src/main/java/com/example/springlocalstackdemo/S3Service.java
@@ -0,0 +1,2 @@
+package com.example.springlocalstackdemo;public class S3Service {
+}
---
+++ src/test/java/com/example/springlocalstackdemo/SpringLocalstackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlocalstackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLocalstackDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
